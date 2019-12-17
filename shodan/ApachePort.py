91932d990b402a4c87e861917e13dfa2b3e86151
Econ <48209216+SmithEcon@users.noreply.github.com>
2019-12-17 18:55:04 +0800

    Create ApachePort.py

---
+++ shodan/ApachePort.py
@@ -0,0 +1,19 @@
+import shodan
+import sys
+
+api=shodan.Shodan("cB9sXwb7l95ZhSJaNgcaO7NQpkzfhQVM")  
+try:
+    results=api.search('apache')    
+    print(results)
+    print("Results found:%s"%results['total'])
+    for result in results['matches']:
+        url = result['ip_str'] + ":" + str(result['port'])
+        print(url)
+
+except KeyboardInterrupt:
+    print ("Ctrl-c pressed ...")
+    sys.exit(1)
+
+except Exception as e:
+    print('Error: %s' % e)
+    sys.exit(1)
